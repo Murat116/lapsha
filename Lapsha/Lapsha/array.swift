b43fc51c0dc953d5ee3392060f7d61f8a89b2c12
Murat116 <89196242960@mail.ru>
2019-07-29 14:21:12 +0300

    улучшенная группировка событий бинарным поиском

---
+++ Lapsha/Lapsha/array.swift
@@ -0,0 +1,63 @@
+//
+//  array.swift
+//  Lapsha
+//
+//  Created by Мурат Камалов on 26/07/2019.
+//  Copyright © 2019 Мурат Камалов. All rights reserved.
+//
+
+import Foundation
+
+
+var array: Array =
+    [
+        ["start": 10, "end" : 20], //1
+        ["start": 50, "end" : 90], //2
+        ["start": 100, "end" : 150 ], //3 +
+        ["start": 105, "end" : 750], //4 + 645
+        ["start": 110, "end" : 150], //5 +
+        ["start": 130, "end" : 710], //6 + 580
+        ["start": 400, "end" : 600 ], //7 +
+        ["start": 610, "end" : 650 ],  //8 40
+        ["start" : 700, "end" : 1200], //9 + 500
+        ["start" : 1300, "end" : 1400], //10 + 7000
+        ["start" : 1500, "end" : 1600], //11
+        ["start" : 1700, "end" : 1800], //12
+        ["start" : 1900, "end" : 2000],//13
+        ["start" : 1950, "end": 3000],
+        ["start" : 2100, "end": 3060],
+        ["start" : 3100, "end":3200],
+       
+       
+]
+//[3, 5, 8, 6, 2, 7, 4]
+        /*["start": 1, "end" : 2], //0
+        ["start": 10, "end" : 20], //1
+        ["start": 50, "end" : 60], //2
+        ["start": 100, "end" : 150 ], //3 +
+        ["start": 105, "end" : 650], //4 +
+        ["start": 110, "end" : 150], //5 +
+        ["start": 130, "end" : 710], //6 +
+        ["start": 400, "end" : 600 ], //7 +
+        ["start": 610, "end" : 650 ],  //8
+        ["start" : 700, "end" : 1200] //9 +
+]
+    
+    ["start": -400, "end": -300],
+    ["start": 1, "end" : 2], //0
+    ["start": 1, "end" : 10],
+    ["start": 1, "end" : 100],
+    ["start": 2, "end" : 102],
+    ["start": 4, "end" : 103],
+    ["start": 5, "end" : 90],
+    ["start": 10, "end" : 20], //1
+    ["start": 50, "end" : 110], //2
+    ["start": 100, "end" : 150 ], //3 +
+    ["start": 105, "end" : 750], //4 + 645
+    ["start": 110, "end" : 150], //5 +
+    ["start": 130, "end" : 710], //6 + 580
+    ["start": 400, "end" : 600 ], //7 +
+    ["start": 610, "end" : 650 ],  //8 40
+    ["start" : 700, "end" : 1200], //9 + 500
+]
+*/
